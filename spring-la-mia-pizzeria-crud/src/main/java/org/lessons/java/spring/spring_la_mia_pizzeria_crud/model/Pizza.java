45827a154da32057094f2ea912914a8385ed0eb0
roccogiordano <rocco.giordano573@gmail.com>
2025-02-25 22:24:46 +0100

    feat: added Pizza model and repository with database configuration

---
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,91 @@
+package org.lessons.java.spring.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotBlank(message = "Name is Mandatory")
+    private String name;
+    
+    @Column(name = "description")
+    @NotBlank(message = "Description is Mandatory")
+    private String description;
+
+    @Column(name = "price")
+    @NotNull(message = "Price cannot be null")
+    @Min(value = 0, message = "Price cannot be less than 0")
+    private Double price;
+
+    @Column(name = "image_url")
+    @NotBlank(message = "Image URL is Mandatory")
+    private String imageUrl;
+
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
