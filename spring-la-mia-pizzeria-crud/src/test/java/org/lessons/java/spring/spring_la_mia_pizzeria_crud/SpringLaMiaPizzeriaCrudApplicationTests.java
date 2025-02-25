1b620f2b3511f2655525b480e831b4d21691cb4b
roccogiordano <rocco.giordano573@gmail.com>
2025-02-25 20:53:07 +0100

    feat: added project scaffholding

---
+++ spring-la-mia-pizzeria-crud/src/test/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring.spring_la_mia_pizzeria_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
