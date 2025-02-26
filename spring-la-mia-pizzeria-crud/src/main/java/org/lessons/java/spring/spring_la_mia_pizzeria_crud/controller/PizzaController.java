863ca96cb4bbce760c83390cf5093ff936ae01e9
roccogiordano <rocco.giordano573@gmail.com>
2025-02-26 23:04:10 +0100

    feat: added pizza search functionality and show in PizzaController

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -6,18 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/pizzas")
 public class PizzaController {
 
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    @GetMapping("/pizzas")
+    @GetMapping
     public String getPizzas(Model model) {
         List<Pizza> pizzas = pizzaRepository.findAll();
+        model.addAttribute("pizzas", pizzas);
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getPizzaById(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).get());
+        return "/show";
+        
+    }   
+
+    @GetMapping("/search")
+    public String findPizzaByName(@RequestParam(name = "name") String name, Model model) {
+        List<Pizza> pizzas = pizzaRepository.findByNameContaining(name);
         model.addAttribute("pizzas", pizzas);
         return "index";
     }
