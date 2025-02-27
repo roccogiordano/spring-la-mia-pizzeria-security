ef4a238e2c108e5f92790a53b4068b6226a748d4
roccogiordano <rocco.giordano573@gmail.com>
2025-02-27 22:24:28 +0100

    feat: added edit, store and delete methods

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/OfferController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/OfferController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -29,8 +31,41 @@ public class OfferController {
 
         offerRepository.save(offer);
 
-        redirectAttributes.addFlashAttribute("message", String.format("Offer %s created successfully", offer.getName()));
+        redirectAttributes.addFlashAttribute("message", String.format("Offer %s has been successfully added to Pizza %s!", offer.getName(), offer.getPizza().getName()));
         redirectAttributes.addFlashAttribute("messageClass", "alert-success");
+
+        return "redirect:/pizzas";
+    }
+
+        @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("offer", offerRepository.findById(id).get());
+        return "/offers/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("offer") Offer offer, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "/offers/edit";
+        }
+
+        offerRepository.save(offer);
+
+        redirectAttributes.addFlashAttribute("message", String.format("Offer %s, for Pizza %s, has been successfully edited!", offer.getName(), offer.getPizza().getName()));
+        redirectAttributes.addFlashAttribute("messageClass", "alert-info");
+
+        return "redirect:/pizzas";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+
+        Offer offer = offerRepository.findById(id).get();
+        offerRepository.delete(offer);
+
+        redirectAttributes.addFlashAttribute("message", String.format("Offer %s has been succesfully deleted!", offer.getName()));
+        redirectAttributes.addFlashAttribute("messageClass", "alert-danger");
 
         return "redirect:/pizzas";
     }
