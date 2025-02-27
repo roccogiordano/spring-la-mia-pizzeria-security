ff8443f10ddd07d09376e063bd77c6e8b2ac018b
roccogiordano <rocco.giordano573@gmail.com>
2025-02-27 01:29:27 +0100

    feat: added pizza create functionality with validation and form

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -5,10 +5,15 @@ import org.lessons.java.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -39,4 +44,22 @@ public class PizzaController {
         model.addAttribute("pizzas", pizzas);
         return "index";
     }
+
+    @GetMapping("/create")
+    public String createPizza(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "create";
+    }
+ 
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza pizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "create";
+        }
+        
+        pizzaRepository.save(pizza);
+        return "redirect:/pizzas";
+    }
+
 }
