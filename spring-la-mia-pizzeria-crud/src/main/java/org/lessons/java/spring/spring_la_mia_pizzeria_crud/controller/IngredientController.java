d45a911d7e382382499ecfc4a0e08bbc0f329adf
roccogiordano <rocco.giordano573@gmail.com>
2025-03-04 15:23:04 +0100

    feat: added create and store methods

--- spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -7,8 +7,14 @@ import org.lessons.java.spring.spring_la_mia_pizzeria_crud.repository.Ingredient
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -24,4 +30,25 @@ public class IngredientController {
         return "/ingredients/index";
     }
     
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
+        return "/ingredients/create";
+    }
+ 
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingredient") Ingredient ingredient, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "/ingredients/create";
+        }
+        
+        ingredientRepository.save(ingredient);
+
+        redirectAttributes.addFlashAttribute("message", String.format("Ingredient %s has been succesfully added to the menu!", ingredient.getName()));
+        redirectAttributes.addFlashAttribute("messageClass", "alert-success");
+
+        return "redirect:/ingredients";
+    }
+
 }
