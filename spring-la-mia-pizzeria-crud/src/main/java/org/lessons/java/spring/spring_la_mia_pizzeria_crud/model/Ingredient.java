549c404181117a2285b81895f006132e62d658a0
roccogiordano <rocco.giordano573@gmail.com>
2025-03-04 15:02:22 +0100

    feat: added Ingredient entity

---
+++ spring-la-mia-pizzeria-crud/src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/model/Ingredient.java
@@ -0,0 +1,60 @@
+package org.lessons.java.spring.spring_la_mia_pizzeria_crud.model;
+
+import io.micrometer.common.lang.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Column(name = "allergens")
+    @Nullable
+    private String allergens;
+
+    
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(String allergens) {
+        this.allergens = allergens;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient [id=" + id + ", name=" + name + ", allergens=" + allergens + "]";
+    }
+
+}
